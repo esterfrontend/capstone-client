c746775eb0e40f985c40245cca4c4d860056d5e2
Ester Martínez <ester.frontend@gmail.com>
2024-03-27 14:40:11 +0100

    Muestra solo los campos con datos en la página de caso y la info de places

--- src/Components/CaseDetails/CaseDetails.jsx
+++ src/Components/CaseDetails/CaseDetails.jsx
@@ -4,29 +4,69 @@ const CaseDetails = ({caseDetails, ...props}) => {
     return (
         <Box {...props}>
             <p><strong>Estado:</strong> {caseDetails.state}</p>
-            <div>
-                <p><strong>Víctima</strong></p>
-                <p>{caseDetails.victim}</p>
-            </div>
-            <div>
-                <p><strong>Dónde sufre acoso</strong></p>
-                <p>{caseDetails.place}</p>
-            </div>
-            <div>
-                <p><strong>Cómo sufre acoso</strong></p>
-                <p>{caseDetails.how}</p>
-            </div>
-            <div>
-                <p><strong>Atacantes</strong></p>
-                <p>{caseDetails.attacker}</p>
-            </div>
-            <div>
-                <p><strong>Más información</strong></p>
-                <p>{caseDetails.moreInformation}</p>
-            </div>
+
+            { caseDetails.victim
+                ? (
+                <div>
+                    <p><strong>Víctima</strong></p>
+                    <p>{caseDetails.victim}</p>
+                </div>
+                ): <></>
+            }
+
+            { caseDetails.place
+                ? (
+                <div>
+                    <p><strong>Dónde sufre acoso</strong></p>
+                    {
+                        caseDetails.place.atSchool
+                            ? <p>En el colegio</p>
+                            : <></>
+                    }
+                    {
+                        caseDetails.place.outside
+                            ? <p>Fuera del colegio</p>
+                            : <></>
+                    }
+                    {
+                        caseDetails.place.socialMedia
+                            ? <p>El las redes sociales</p>
+                            : <></>
+                    }
+                </div>
+                ): <></>
+            }
+
+            { caseDetails.how
+                ? (
+                <div>
+                    <p><strong>Cómo sufre acoso</strong></p>
+                    <p>{caseDetails.how}</p>
+                </div>
+                ): <></>
+            }
+
+            { caseDetails.attacker
+                ? (
+                <div>
+                    <p><strong>Atacantes</strong></p>
+                    <p>{caseDetails.attacker}</p>
+                </div>
+                ): <></>
+            }
+
+            { caseDetails.moreInformation
+                ? (
+                <div>
+                    <p><strong>Más información</strong></p>
+                    <p>{caseDetails.moreInformation}</p>
+                </div>
+                ): <></>
+            }
+
             <div>
                 <p><strong>Informante</strong></p>
-                <p>{caseDetails.informant.anonymous === true ? 'Anónimo' : `${caseDetails?.informant.name}, ${caseDetails?.informant.relation}, ${caseDetails?.informant.contact}`}</p>
+                <p>{caseDetails.informant.anonymous === true ? 'Anónimo' : caseDetails?.informant.name}</p>
             </div>
         </Box>
     )
