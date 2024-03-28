4fa19b14b936ed66611c3803313b5883f7fd17d2
Ester Martínez <ester.frontend@gmail.com>
2024-03-28 23:50:44 +0100

    En el menú de perfil soluciona errore de ruta, rol y key única

--- src/Components/ProfileNavigation/ProfileNavigation.jsx
+++ src/Components/ProfileNavigation/ProfileNavigation.jsx
@@ -2,7 +2,7 @@ import { Box, Flex } from "@chakra-ui/react"
 import { useLocation } from 'react-router-dom'
 import './ProfileNavigation.css'
 import CustomLink from "../CustomLink/CustomLink"
-import { useContext } from "react"
+import React, { useContext, Fragment } from "react"
 import { AuthContext } from "../../contexts/AuthContext"
 
 const ProfileNavigation = () => {
@@ -20,40 +20,40 @@ const ProfileNavigation = () => {
         {
             text: 'Psicólogo asociado',
             link: '/psicologo-asociado',
-            roleHidden: 'psicologo'
+            roleHidden: 'profesional'
         },
         {
             text: 'Colegios asociados',
-            link: '/colegios-asociado',
+            link: '/colegios-asociados',
             roleHidden: 'colegio'
         }
     ]
     const location = useLocation()
 
+    
     return (
         <Flex className={'profile-navigation'} direction={'column'} bgColor={'brand.pinkUltraLight'} padding={'5px 20px'} margin={'0px'} listStyleType={'none'} minW={'250px'}>
-            { PROFILE_NAVIGATION.map(({ link, text, roleHidden }) => {
+            { PROFILE_NAVIGATION.map(({ link, text, roleHidden }, index) => {
                 const isActiveLink = location.pathname === link
 
                 return user.role === roleHidden
-                    ? <></>
+                    ? <React.Fragment key={link}></React.Fragment>
                     : (
-                            <CustomLink 
-                                to={link} 
-                                key={text} 
-                                color={isActiveLink ? "brand.primary" : "brand.black"}
-                                fontWeight={isActiveLink ? "600" : "400"}
-                                _hover={{
-                                    color: "brand.primary",
-                                    fontWeight: '600',
-                                    textDecoration:'none'
-                                }}
-                                display={'inline-block'} padding={'20px 10px'} borderBottom={'1px solid'} borderBottomColor={'brand.pink'}
-                            >
-                                {text}
-                            </CustomLink>
-                        )
-                 
+                        <CustomLink 
+                            to={link} 
+                            key={index} 
+                            color={isActiveLink ? "brand.primary" : "brand.black"}
+                            fontWeight={isActiveLink ? "600" : "400"}
+                            _hover={{
+                                color: "brand.primary",
+                                fontWeight: '600',
+                                textDecoration:'none'
+                            }}
+                            display={'inline-block'} padding={'20px 10px'} borderBottom={'1px solid'} borderBottomColor={'brand.pink'}
+                        >
+                            {text}
+                        </CustomLink>
+                    ) 
             })} 
         </Flex>
     )
