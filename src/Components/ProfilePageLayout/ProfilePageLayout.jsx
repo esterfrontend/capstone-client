ecb19733c13c7511eca4222261e8ebf44e770d90
Ester Martínez <ester.frontend@gmail.com>
2024-03-28 23:52:13 +0100

    Cambia estilos del layout general de perfil

--- src/Components/ProfilePageLayout/ProfilePageLayout.jsx
+++ src/Components/ProfilePageLayout/ProfilePageLayout.jsx
@@ -4,11 +4,11 @@ import ProfileNavigation from "../../Components/ProfileNavigation/ProfileNavigat
 
 const ProfilePageLayout = ({children, pageTitle}) => {
     return (
-        <PageWrapper className='container'>
-            <Flex gap={'60px'}>
+        <PageWrapper className='container' mb={'60px'}>
+            <Flex gap={'60px'} alignItems={'flex-start'}>
                 <ProfileNavigation />
-                <Box>
-                    <Heading as='h1'>{ pageTitle }</Heading>
+                <Box flex={'1'}>
+                    <Heading as='h1' mb={'30px'}>{ pageTitle }</Heading>
                     {children}
                 </Box>
             </Flex>
