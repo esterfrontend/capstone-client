9b6f406a1d79ed08c3a883b154fd528c2ec4251f
Ester Martínez <ester.frontend@gmail.com>
2024-03-28 21:57:29 +0100

    Hace que el menú de perfil cambie dependiendo del rol de usuario

--- src/Components/ProfileNavigation/ProfileNavigation.jsx
+++ src/Components/ProfileNavigation/ProfileNavigation.jsx
@@ -2,8 +2,12 @@ import { Box, Flex } from "@chakra-ui/react"
 import { useLocation } from 'react-router-dom'
 import './ProfileNavigation.css'
 import CustomLink from "../CustomLink/CustomLink"
+import { useContext } from "react"
+import { AuthContext } from "../../contexts/AuthContext"
 
 const ProfileNavigation = () => {
+    const { user } = useContext(AuthContext)
+    
     const PROFILE_NAVIGATION = [
         {
             text: 'Mi perfil',
@@ -15,32 +19,41 @@ const ProfileNavigation = () => {
         },
         {
             text: 'Psicólogo asociado',
-            link: '/psicologo-asociado'
+            link: '/psicologo-asociado',
+            roleHidden: 'psicologo'
+        },
+        {
+            text: 'Colegios asociados',
+            link: '/colegios-asociado',
+            roleHidden: 'colegio'
         }
     ]
     const location = useLocation()
 
     return (
         <Flex className={'profile-navigation'} direction={'column'} bgColor={'brand.pinkUltraLight'} padding={'5px 20px'} margin={'0px'} listStyleType={'none'} minW={'250px'}>
-            { PROFILE_NAVIGATION.map(({ link, text }) => {
+            { PROFILE_NAVIGATION.map(({ link, text, roleHidden }) => {
                 const isActiveLink = location.pathname === link
-             
-                return (
-                    <CustomLink 
-                        to={link} 
-                        key={text} 
-                        color={isActiveLink ? "brand.primary" : "brand.black"}
-                        fontWeight={isActiveLink ? "600" : "400"}
-                        _hover={{
-                            color: "brand.primary",
-                            fontWeight: '600',
-                            textDecoration:'none'
-                        }}
-                        display={'inline-block'} padding={'20px 10px'} borderBottom={'1px solid'} borderBottomColor={'brand.pink'}
-                    >
-                        {text}
-                    </CustomLink>
-                )
+
+                return user.role === roleHidden
+                    ? <></>
+                    : (
+                            <CustomLink 
+                                to={link} 
+                                key={text} 
+                                color={isActiveLink ? "brand.primary" : "brand.black"}
+                                fontWeight={isActiveLink ? "600" : "400"}
+                                _hover={{
+                                    color: "brand.primary",
+                                    fontWeight: '600',
+                                    textDecoration:'none'
+                                }}
+                                display={'inline-block'} padding={'20px 10px'} borderBottom={'1px solid'} borderBottomColor={'brand.pink'}
+                            >
+                                {text}
+                            </CustomLink>
+                        )
+                 
             })} 
         </Flex>
     )
