c462f7d5be7dc7b031ef3096dafbc05575ddbbc9
Ester Martínez <ester.frontend@gmail.com>
2024-03-30 21:10:59 +0100

    Crea botón con variante red en el tema

--- src/theme/components/button.jsx
+++ src/theme/components/button.jsx
@@ -1,28 +1,35 @@
 import { defineStyleConfig } from '@chakra-ui/react'
 
 const Button = defineStyleConfig({
-  baseStyle: {
-    fontWeight: 'bold',
-    textTransform: 'uppercase',
-    borderRadius: '0px'
-  },
-  variants: {
-    solid: {
-      bg: 'brand.primary',
-      color: 'white',
-      _hover: {
-        bg:'brand.primaryDark'
-    }
+    baseStyle: {
+        fontWeight: 'bold',
+        textTransform: 'uppercase',
+        borderRadius: '0px'
     },
-    empty: {
-        bg: 'transparent',
-        color: 'brand.black'
-    }
-  },
-  defaultProps: {
-    size: 'md',
-    variant: 'solid',
-  },
+    variants: {
+        solid: {
+            bg: 'brand.primary',
+            color: 'white',
+            _hover: {
+                bg: 'brand.primaryDark'
+            }
+        },
+        empty: {
+            bg: 'transparent',
+            color: 'brand.black'
+        },
+        red: {
+            bg: 'red.500',
+            color: 'white',
+            _hover: {
+                bg: 'red.800'
+            }
+        }
+    },
+    defaultProps: {
+        size: 'md',
+        variant: 'solid',
+    },
 })
 
 export default Button
