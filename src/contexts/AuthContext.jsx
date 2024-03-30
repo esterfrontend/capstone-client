423e80c3244159a958f7d45be00631652f26cf35
Ester Martínez <ester.frontend@gmail.com>
2024-03-30 21:10:08 +0100

    Quita getToken como exportación de AuthProvider

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -69,7 +69,7 @@ export const AuthProvider = ({ children }) => {
     }, [])
 
     return (
-        <AuthContext.Provider value={{ user, logout, login, isLoading, getToken, removeUser }}>
+        <AuthContext.Provider value={{ user, logout, login, isLoading, removeUser }}>
             {children}
         </AuthContext.Provider>
     )
