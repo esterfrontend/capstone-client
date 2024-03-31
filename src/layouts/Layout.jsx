1f1f1951d478daa2f722d9a8d4b620f0e02fd28a
Ester Martínez <ester.frontend@gmail.com>
2024-03-31 14:13:37 +0200

    Coloca el footer abajo de la pantalla

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -2,13 +2,18 @@ import Navbar from "../Components/Navbar/Navbar"
 import { Outlet } from "react-router-dom"
 import { AuthProvider } from "../contexts/AuthContext"
 import Footer from "../Components/Footer/Footer"
+import { Box, Flex } from "@chakra-ui/react"
 
 const Layout = () => {
     return (
         <AuthProvider>
-            <Navbar />
-            <Outlet />
-            <Footer />
+            <Flex minH={'100vh'} flexDir={'column'}>
+                <Navbar />
+                <Box flex={'1'}>
+                    <Outlet />
+                </Box>
+                <Footer />
+            </Flex>
         </AuthProvider>
     )
 }
