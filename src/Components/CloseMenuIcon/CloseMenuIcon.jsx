29a6f4fdf58e40a5ea086298ef9d45acb6955238
Ester Martínez <ester.frontend@gmail.com>
2024-04-01 16:18:17 +0200

    Fix: errores en consola de svg

--- src/Components/CloseMenuIcon/CloseMenuIcon.jsx
+++ src/Components/CloseMenuIcon/CloseMenuIcon.jsx
@@ -2,7 +2,7 @@ const CloseMenuIcon = () => {
     return (
         <svg width="30" height="27" viewBox="0 0 30 27" fill="none" xmlns="http://www.w3.org/2000/svg">
             <rect width="30" height="27" rx="4" fill="#FDE1D8"/>
-            <path d="M8 8L22 19M8 19L22 8" stroke="#3C3C3C" stroke-linecap="round"/>
+            <path d="M8 8L22 19M8 19L22 8" stroke="#3C3C3C" strokeLinecap="round"/>
         </svg>
     )
 }
--- src/Components/HamburgerIcon/HamburgerIcon.jsx
+++ src/Components/HamburgerIcon/HamburgerIcon.jsx
@@ -2,7 +2,7 @@ const HamburgerIcon = () => {
     return (
         <svg width="30" height="27" viewBox="0 0 30 27" fill="none" xmlns="http://www.w3.org/2000/svg">
             <rect width="30" height="27" rx="4" fill="#FDE1D8"/>
-            <path d="M8 8H22M8 13.5H22M8 19H22" stroke="#3C3C3C" stroke-linecap="round"/>
+            <path d="M8 8H22M8 13.5H22M8 19H22" stroke="#3C3C3C" strokeLinecap="round"/>
         </svg>
     )
 }
