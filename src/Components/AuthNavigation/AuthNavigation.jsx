9d835d8a73cf5bbda35e35af647664d9e8a2d1ff
Ester Martínez <ester.frontend@gmail.com>
2024-04-01 20:44:57 +0200

    Crea dropdown menu en Mi perfil

--- src/Components/AuthNavigation/AuthNavigation.jsx
+++ src/Components/AuthNavigation/AuthNavigation.jsx
@@ -1,7 +1,9 @@
 import { useContext } from "react"
 import { AuthContext } from "../../contexts/AuthContext"
 import AuthLink from '../AuthLink/AuthLink'
-import { Flex } from "@chakra-ui/react"
+import { Box, Flex } from "@chakra-ui/react"
+import './AuthNavigation.css'
+import CustomLink from "../CustomLink/CustomLink"
 
 const AuthNavigation = ({...props}) => {
     const { user, logout } = useContext(AuthContext)
@@ -10,7 +12,29 @@ const AuthNavigation = ({...props}) => {
         <Flex {...props}>
             { user ? (
                 <>
-                    <AuthLink to={"/mi-perfil"}>Mi perfil</AuthLink>
+                    <Box position={'relative'} className="dropdown-menu">
+                        <AuthLink to={"/mi-perfil"}>Mi perfil</AuthLink>
+                        <Flex className="dropdown-menu__list" 
+                            position={'absolute'}
+                            top={'22px'}
+                            right={'0'} 
+                            width={'max-content'}
+                            flexDirection={'column'}
+                            textAlign={'left'}
+                            bgColor={'white'}
+                            border={'2px solid'}
+                            borderColor={'brand.pinkLight'}
+                        >
+                            <CustomLink variant={'empty'} to={"/mi-perfil"}>Mi perfil</CustomLink>
+                            <CustomLink variant={'empty'} to={"/mis-casos"}>Mis casos</CustomLink>
+                            {user.role === 'colegio' &&
+                                <CustomLink variant={'empty'} to={"/psicologo-asociado"}>Psicólogo asociado</CustomLink>
+                            }
+                            {user.role === 'profesional' &&
+                                <CustomLink variant={'empty'} to={"/colegios-asociados"}>Colegios asociados</CustomLink>
+                            }
+                        </Flex>
+                    </Box>
                     <AuthLink onClick={logout}>Logout</AuthLink>
                 </>
             ) : (
