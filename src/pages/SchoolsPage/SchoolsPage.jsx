6372edb6b01fd13086697e8b156fdaebb95f6426
Ester Martínez <ester.frontend@gmail.com>
2024-04-02 12:09:07 +0200

    Pone mapa sticky en SchoolsPage

--- src/pages/SchoolsPage/SchoolsPage.jsx
+++ src/pages/SchoolsPage/SchoolsPage.jsx
@@ -5,6 +5,7 @@ import { Box, Flex } from "@chakra-ui/react";
 import { useLoaderData } from 'react-router-dom';
 import SignUpLinkBox from '../../Components/SignUpLinkBox/SignUpLinkBox';
 import MyMap from '../../Components/MyMap/MyMap';
+import './SchoolsPage.css'
 
 function SchoolsPage() {
     const schools = useLoaderData()
@@ -18,7 +19,7 @@ function SchoolsPage() {
                     Colegios inscritos
                 </MainHeading>
                 <Flex flexDirection={["column", "column", "row"]}>
-                    <Box w={['100%', '100%', '40%', '40%', '35%']} h={['300px', '300px', 'auto']}>
+                    <Box className={'schools__container-map'} w={['100%', '100%', '40%', '40%', '35%']} h={['300px', '300px', 'auto']}>
                         <MyMap schools={schools}/>
                     </Box>
                     <SchoolsGrid schools={schools} flex={'1'}/>
